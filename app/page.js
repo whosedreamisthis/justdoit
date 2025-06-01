01af69440ae972250bd762992c012fa5644da8b4
Dana Sharon <whosedreamisthis@gmail.com>
2025-05-31 19:36:28 -0700

    expand cards

--- app/page.js
+++ app/page.js
@@ -5,12 +5,21 @@ import habits from '@/data/habits.json';
 import ExploreTab from '@/components/explore-tab';
 export default function App() {
 	const [activeTab, setActiveTab] = useState('explore');
+
+	const onExploreHabitSelected = (habitId) => {
+		console.log('habit selected', habitId);
+	};
 	return (
 		<div className="min-h-screen flex flex-col">
 			{/* Tab Content */}
 			<div className="flex-grow p-4 pb-20">
 				{activeTab === 'goals' && <p>Track current habits</p>}
-				{activeTab === 'explore' && <ExploreTab habits={habits} />}
+				{activeTab === 'explore' && (
+					<ExploreTab
+						habits={habits}
+						onSelect={onExploreHabitSelected}
+					/>
+				)}
 				{activeTab === 'settings' && <p>Manage your settings here!</p>}
 			</div>
 
--- components/explore-tab.jsx
+++ components/explore-tab.jsx
@@ -1,20 +1,16 @@
-export default function ExploreTab({ habits }) {
+import MinimizableCard from '@/components/minimizable-card';
+
+export default function ExploreTab({ habits, onSelect }) {
 	return (
 		<div className="p-6">
 			<h2 className="text-3xl font-bold mb-4">Explore New Habits</h2>
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 				{habits.map((habit) => (
-					<div
+					<MinimizableCard
 						key={habit.id}
-						className="bg-white rounded-lg shadow-lg p-4"
-					>
-						<h2 className="text-lg font-bold text-gray-800">
-							{habit.title}
-						</h2>
-						<p className="text-gray-600 text-sm">
-							{habit.shortDescription}
-						</p>
-					</div>
+						habit={habit}
+						onSelect={onSelect}
+					/>
 				))}
 			</div>
 		</div>

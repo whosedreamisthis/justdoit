0d52c578a911bc336f3d5a0791106571cce85834
Dana Sharon <whosedreamisthis@gmail.com>
2025-06-03 20:53:18 -0700

    clerk sending key

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+		>
 			<html lang="en">
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}

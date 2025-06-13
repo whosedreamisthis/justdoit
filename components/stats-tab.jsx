627514068b8999c541b1c2c4f5a506890ab1835b
Dana Sharon <whosedreamisthis@gmail.com>
2025-06-12 18:42:03 -0700

    stats elements line up

--- components/stats-tab.jsx
+++ components/stats-tab.jsx
@@ -104,7 +104,7 @@ export default function StatsTab({
 							</label>
 							<select
 								id="goal-select"
-								className={`${styles.customSelect} block w-full px-4 py-2 text-base text-gray-900 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm`}
+								className={`${styles.customSelect} block w-full px-4 py-3 text-base text-gray-900 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-primary focus:border-primary sm:text-sm`}
 								value={selectedGoalTitle}
 								onChange={handleSelectChange}
 							>
